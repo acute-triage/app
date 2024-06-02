f745a8e88f1e18e938619bda8a7331fdcedbe3d0
Anja Flædekær <anjaflaedekaer@gmail.com>
2024-06-02 20:11:54 +0200

    feat: fixed contactcard20

--- lib/features/contact_card/data/contact_card_20.dart
+++ lib/features/contact_card/data/contact_card_20.dart
@@ -6,86 +6,97 @@ import 'package:flutter_starter/features/contact_card/domain/sympton_category.da
 const contactCard20 = ContactReasonCard(
   title: 'Spiseproblemer, vægttab og anorexi',
   number: 20,
-  searchTerms: [],
-  // TODO: Add support for multiple choice
-  symptomCategories: [],
-  // symptomCategories: [
-  //   SymptomCategory(
-  //     name: 'Vægttab uden anorexi',
-  //     symptoms: [
-  //       Symptom(
-  //         description: 'Ja',
-  //         code: codeGreen,
-  //       ),
-  //     ],
-  //   ),
-  //   SymptomCategory(
-  //     name: 'Anorexi og mistanke om nedenstående:',
-  //     symptoms: [
-  //       Symptom(
-  //         description: 'Arytmi anden end sinusbradykardi',
-  //         code: codeOrange,
-  //       ),
-  //       Symptom(
-  //         description: 'Synkope',
-  //         code: codeOrange,
-  //       ),
-  //       Symptom(
-  //         description: 'Kramper',
-  //         code: codeOrange,
-  //       ),
-  //       Symptom(
-  //         description: 'Hjerteinsufficiens',
-  //         code: codeOrange,
-  //       ),
-  //       Symptom(
-  //         description: 'Leverinsufficiens',
-  //         code: codeOrange,
-  //       ),
-  //       Symptom(
-  //         description: 'Elektrolytforstyrrelser (målt)',
-  //         code: codeOrange,
-  //       ),
-  //       Symptom(
-  //         description: 'Pancreatitis',
-  //         code: codeYellow,
-  //       ),
-  //       Symptom(
-  //         description: 'Bradykardi under 30',
-  //         code: codeYellow,
-  //       ),
-  //       Symptom(
-  //         description:
-  //             'Bradykardi under 40 kombineret med hypotension (sys-BT under 80) eller svimmelhed',
-  //         code: codeYellow,
-  //       ),
-  //       Symptom(
-  //         description: 'Temperatur under 35.0 °C',
-  //         code: codeYellow,
-  //       ),
-  //     ],
-  //   ),
-  //   SymptomCategory(
-  //     name: '“Refeeding syndrom” mistanke med a) eller b)',
-  //     symptoms: [
-  //       Symptom(
-  //         description: 'Udtalte ødemer',
-  //         code: codeOrange,
-  //       ),
-  //       Symptom(
-  //         description: 'Serum fosfat under 0.65 mmol/l',
-  //         code: codeOrange,
-  //       ),
-  //     ],
-  //   ),
-  //   SymptomCategory(
-  //     name: 'Vægt for højde under 70% af 50-percentil (forventet)',
-  //     symptoms: [
-  //       Symptom(
-  //         description: 'Ja',
-  //         code: codeGreen,
-  //       ),
-  //     ],
-  //   ),
-  // ],
+  searchTerms: [
+    'spiseforstyrrelse'
+        'arytmi'
+        'bradykardi under 30'
+        'ødemer'
+        'vægt'
+  ],
+  symptomCategories: [
+    SymptomCategory(
+      name: 'Vægttab uden anorexi',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeGreen),
+      ],
+    ),
+    SymptomCategory(
+      name: 'Arytmi anden end sinusbradykardi',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeOrange),
+      ],
+    ),
+    SymptomCategory(
+      name: 'Synkope',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeOrange),
+      ],
+    ),
+    SymptomCategory(
+      name: 'Kramper',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeOrange),
+      ],
+    ),
+    SymptomCategory(
+      name: 'Hjerteinsufficiens',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeOrange),
+      ],
+    ),
+    SymptomCategory(
+      name: 'Leverinsufficiens',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeOrange),
+      ],
+    ),
+    SymptomCategory(
+      name: ' Elektrolytforstyrrelser (målt)',
+      symptoms: [
+        Symptom(description: 'Pancreatitis', code: codeYellow),
+      ],
+    ),
+    SymptomCategory(
+      name: 'Bradykardi under 30',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeYellow),
+      ],
+    ),
+    SymptomCategory(
+      name:
+          'Bradykardi under 40 kombineret med hypotension (sys-BT under 80) eller svimmelhed',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeYellow),
+      ],
+    ),
+    SymptomCategory(
+      name: 'Temperatur under 35.0°C',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeYellow),
+      ],
+    ),
+    SymptomCategory(
+      name:
+          'Refeeding syndrom med mistanke om udtalte ødemer eller serum fosfat under 0.65 mmol/l',
+      symptoms: [Symptom(description: 'Ja', code: codeOrange)],
+    ),
+    SymptomCategory(
+      name: 'Udtalte ødemer',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeOrange),
+      ],
+    ),
+    SymptomCategory(
+      name: 'Serum fosfat under 0.65 mmol/l',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeOrange),
+      ],
+    ),
+    SymptomCategory(
+      name: 'Vægt for højde under 70% af 50-percentil (forventet)',
+      symptoms: [
+        Symptom(description: 'Ja', code: codeGreen),
+      ],
+    ),
+  ],
 );
