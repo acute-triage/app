1bf2327f25012d0329d411380022ef56da891e85
Mattias Siø Fjellvang <mattias@doubble.dk>
2024-06-03 11:54:14 +0200

    refactor: removed unused file

--- lib/common/application/router.gr.dart
+++ lib/common/application/router.gr.dart
@@ -8,105 +8,54 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i5;
-import 'package:flutter/material.dart' as _i6;
+import 'package:auto_route/auto_route.dart' as _i4;
+import 'package:flutter/material.dart' as _i5;
 import 'package:flutter_starter/common/presentation/screens/home_screen.dart'
-    as _i3;
-import 'package:flutter_starter/common/presentation/screens/splash_screen.dart'
-    as _i4;
-import 'package:flutter_starter/features/contact_card/domain/contact_reason_card.dart'
-    as _i7;
-import 'package:flutter_starter/features/contact_card/presentation/screens/contact_card_category_screen.dart'
-    as _i1;
-import 'package:flutter_starter/features/contact_card/presentation/screens/contact_card_screen.dart'
     as _i2;
+import 'package:flutter_starter/common/presentation/screens/splash_screen.dart'
+    as _i3;
+import 'package:flutter_starter/features/contact_card/domain/contact_reason_card.dart'
+    as _i6;
+import 'package:flutter_starter/features/contact_card/presentation/screens/contact_card_screen.dart'
+    as _i1;
 
-abstract class $AppRouter extends _i5.RootStackRouter {
+abstract class $AppRouter extends _i4.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i5.PageFactory> pagesMap = {
-    ContactCardCategoryRoute.name: (routeData) {
-      final args = routeData.argsAs<ContactCardCategoryRouteArgs>();
-      return _i5.AutoRoutePage<dynamic>(
-        routeData: routeData,
-        child: _i1.ContactCardCategoryScreen(
-          key: args.key,
-          contactCard: args.contactCard,
-        ),
-      );
-    },
+  final Map<String, _i4.PageFactory> pagesMap = {
     ContactCardRoute.name: (routeData) {
       final args = routeData.argsAs<ContactCardRouteArgs>();
-      return _i5.AutoRoutePage<dynamic>(
+      return _i4.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: _i2.ContactCardScreen(
+        child: _i1.ContactCardScreen(
           key: args.key,
           contactCard: args.contactCard,
         ),
       );
     },
     HomeRoute.name: (routeData) {
-      return _i5.AutoRoutePage<dynamic>(
+      return _i4.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i3.HomeScreen(),
+        child: const _i2.HomeScreen(),
       );
     },
     SplashRoute.name: (routeData) {
-      return _i5.AutoRoutePage<dynamic>(
+      return _i4.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i4.SplashScreen(),
+        child: const _i3.SplashScreen(),
       );
     },
   };
 }
 
 /// generated route for
-/// [_i1.ContactCardCategoryScreen]
-class ContactCardCategoryRoute
-    extends _i5.PageRouteInfo<ContactCardCategoryRouteArgs> {
-  ContactCardCategoryRoute({
-    _i6.Key? key,
-    required _i7.ContactReasonCard contactCard,
-    List<_i5.PageRouteInfo>? children,
-  }) : super(
-          ContactCardCategoryRoute.name,
-          args: ContactCardCategoryRouteArgs(
-            key: key,
-            contactCard: contactCard,
-          ),
-          initialChildren: children,
-        );
-
-  static const String name = 'ContactCardCategoryRoute';
-
-  static const _i5.PageInfo<ContactCardCategoryRouteArgs> page =
-      _i5.PageInfo<ContactCardCategoryRouteArgs>(name);
-}
-
-class ContactCardCategoryRouteArgs {
-  const ContactCardCategoryRouteArgs({
-    this.key,
-    required this.contactCard,
-  });
-
-  final _i6.Key? key;
-
-  final _i7.ContactReasonCard contactCard;
-
-  @override
-  String toString() {
-    return 'ContactCardCategoryRouteArgs{key: $key, contactCard: $contactCard}';
-  }
-}
-
-/// generated route for
-/// [_i2.ContactCardScreen]
-class ContactCardRoute extends _i5.PageRouteInfo<ContactCardRouteArgs> {
+/// [_i1.ContactCardScreen]
+class ContactCardRoute extends _i4.PageRouteInfo<ContactCardRouteArgs> {
   ContactCardRoute({
-    _i6.Key? key,
-    required _i7.ContactReasonCard contactCard,
-    List<_i5.PageRouteInfo>? children,
+    _i5.Key? key,
+    required _i6.ContactReasonCard contactCard,
+    List<_i4.PageRouteInfo>? children,
   }) : super(
           ContactCardRoute.name,
           args: ContactCardRouteArgs(
@@ -118,8 +67,8 @@ class ContactCardRoute extends _i5.PageRouteInfo<ContactCardRouteArgs> {
 
   static const String name = 'ContactCardRoute';
 
-  static const _i5.PageInfo<ContactCardRouteArgs> page =
-      _i5.PageInfo<ContactCardRouteArgs>(name);
+  static const _i4.PageInfo<ContactCardRouteArgs> page =
+      _i4.PageInfo<ContactCardRouteArgs>(name);
 }
 
 class ContactCardRouteArgs {
@@ -128,9 +77,9 @@ class ContactCardRouteArgs {
     required this.contactCard,
   });
 
-  final _i6.Key? key;
+  final _i5.Key? key;
 
-  final _i7.ContactReasonCard contactCard;
+  final _i6.ContactReasonCard contactCard;
 
   @override
   String toString() {
@@ -139,9 +88,9 @@ class ContactCardRouteArgs {
 }
 
 /// generated route for
-/// [_i3.HomeScreen]
-class HomeRoute extends _i5.PageRouteInfo<void> {
-  const HomeRoute({List<_i5.PageRouteInfo>? children})
+/// [_i2.HomeScreen]
+class HomeRoute extends _i4.PageRouteInfo<void> {
+  const HomeRoute({List<_i4.PageRouteInfo>? children})
       : super(
           HomeRoute.name,
           initialChildren: children,
@@ -149,13 +98,13 @@ class HomeRoute extends _i5.PageRouteInfo<void> {
 
   static const String name = 'HomeRoute';
 
-  static const _i5.PageInfo<void> page = _i5.PageInfo<void>(name);
+  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i4.SplashScreen]
-class SplashRoute extends _i5.PageRouteInfo<void> {
-  const SplashRoute({List<_i5.PageRouteInfo>? children})
+/// [_i3.SplashScreen]
+class SplashRoute extends _i4.PageRouteInfo<void> {
+  const SplashRoute({List<_i4.PageRouteInfo>? children})
       : super(
           SplashRoute.name,
           initialChildren: children,
@@ -163,5 +112,5 @@ class SplashRoute extends _i5.PageRouteInfo<void> {
 
   static const String name = 'SplashRoute';
 
-  static const _i5.PageInfo<void> page = _i5.PageInfo<void>(name);
+  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
 }
--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 30 (15 per locale)
 ///
-/// Built on 2024-06-02 at 19:45 UTC
+/// Built on 2024-06-03 at 09:48 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
