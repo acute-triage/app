ee817ea9f22bc1c7dd84a401552386b80842ec70
Mattias Siø Fjellvang <mattias@doubble.dk>
2024-06-03 21:29:22 +0200

    chore: bumped kotlin version

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 30 (15 per locale)
 ///
-/// Built on 2024-06-03 at 18:56 UTC
+/// Built on 2024-06-03 at 19:28 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
